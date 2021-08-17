b416cc4e044873042482d10706cf1c3a2b62bb45
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-08-17 16:10:14 +0700

    update file

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ function sayHi($name, $age){ //có thể khai báo nhiều biến trong một fu
 sayHi("Huu", "6"); //gọi hàm sayHi để thực thi
 sayHi("Vinh", "3");
 sayHi("Giang", "5"); // có thể dùng lại nhiều lần
+
+// return
+
+function cube($num){
+    return $num * $num * $num;
+    echo "hell"; //return sẽ chấm dứt ngay function, những gì ở sau return sẽ k được thực thi
+
+}
+
+$result = cube(4);
+echo $result;
