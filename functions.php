8d0e05358e965611eabf1900b405ad1ced84cf3d
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-08-17 15:22:10 +0700

    function file

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function sayHi($name, $age){
+    echo "Hello $name, $age tuổi <br>";
+}
+
+sayHi("Huu", "6"); //gọi hàm sayHi để thực thi
+sayHi("Vinh", "3");
+sayHi("Giang", "5"); // có thể dùng lại nhiều lần
