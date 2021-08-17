59682dd124c58a5aa7b35d1199df50c4b722b01c
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-08-17 15:51:20 +0700

    update

--- array.php
+++ array.php
@@ -4,5 +4,5 @@ $friends = array('Hữu', 'Vinh', 'Giang', 'Đạt');
 $friends[1] = 'Hữu Ngậu';
 echo $friends[0];
 echo "<br>";
-var_dump($friends);  //đây là ghi chú ok
+var_dump($friends);  //đây là ghi chú ok okgit
 
