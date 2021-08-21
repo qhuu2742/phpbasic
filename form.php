dfb98cbd5b3b4e29b7d425d936fa9d373de42a68
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-08-21 23:40:51 +0700

    update

--- form.php
+++ form.php
@@ -11,11 +11,3 @@
 </body>
 </html>
 
-<!--<html>-->
-<!--<body>-->
-<!---->
-<!--Welcome --><?php //echo $_GET["name"]; ?><!--<br>-->
-<!--Your email address is: --><?php //echo $_GET["email"]; ?>
-<!---->
-<!--</body>-->
-<!--</html>-->
