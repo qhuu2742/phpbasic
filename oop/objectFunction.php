e9d3600592c434399621ff0b8be828c7322a42ec
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-08-24 21:45:04 +0700

    update file

--- oop/objectFunction.php
+++ oop/objectFunction.php
@@ -23,7 +23,15 @@ class Student {
     function get_gpa(){
         return $this->gpa;
     }
+
+    function checkGpa(){
+        if ($this->gpa >= 3.5){
+            return "Bạn được học bổng!";
+        }
+        return "Bạn không được học bổng";
+    }
 }
 
 $student1 = new Student('Hữu', 'CNTT', 4.0);
 echo $student1->name;
+echo $student1->checkGpa();
