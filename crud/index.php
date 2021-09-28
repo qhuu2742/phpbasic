1f65ba6c0a9f2c47d3b7efe59ba79c7a4cab9d5d
Phạm Quang Hữu <qhuu.official@gmail.com>
2021-09-29 03:05:36 +0700

    file index

---
+++ crud/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+        table tr td:last-child{
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="mt-5 mb-3 clearfix">
+                    <h2 class="pull-left">Tất cả sinh viên</h2>
+                    <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Thêm sinh viên</a>
+                </div>
+                <?php
+                // Include config file
+                require_once "config.php";
+
+                // Câu truy vấn
+                $sql = "SELECT * FROM sinh_vien";
+
+                // Thực hiện hai câu truy vấn
+                if($result = $mysqli->query($sql)){
+                    if($result->num_rows > 0){
+                        echo '<table class="table table-bordered table-striped">';
+                            echo "<thead>";
+                                echo "<tr>";
+                                    echo "<th>Mã</th>";
+                                    echo "<th>Họ tên</th>";
+                                    echo "<th>Địa chỉ</th>";
+                                    echo "<th>Học phí</th>";
+                                    echo "<th>Thao tác</th>";
+                                 echo "</tr>";
+                                echo "</thead>";
+                        echo "<tbody>";
+
+                        while($row = $result->fetch_array()){
+                            echo "<tr>";
+                                echo "<td>" . $row['ma'] . "</td>";
+                                echo "<td>" . $row['ho_ten'] . "</td>";
+                                echo "<td>" . $row['dia_chi'] . "</td>";
+                                echo "<td>" . $row['hoc_phi'] . "</td>";
+                                echo "<td>";
+                                    echo '<a href="read.php?ma='. $row['ma'] .'" class="mr-3" title="Xem" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                    echo '<a href="update.php?ma='. $row['ma'] .'" class="mr-3" title="Cập nhật" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                    echo '<a href="delete.php?ma='. $row['ma'] .'" title="Xóa" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                        // Giải phóng
+                        $result->free();
+                    } else{
+                        echo '<div class="alert alert-danger"><em>Không có bản ghi nào</em></div>';
+                    }
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+
+                // Close connection
+                $mysqli->close();
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
